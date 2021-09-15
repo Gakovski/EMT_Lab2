702afb0ad17ba8c3f206d91a26db257ed2a24bed
Gakovski <david_gakovski@outlook.com>
2021-09-15 15:30:49 +0200

    2nd Attempt in dockerizing

--- library_app/src/test/java/com/emt/lab2/Lab2ApplicationTests.java
+++ library_app/src/test/java/com/emt/lab2/Lab2ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.emt.lab2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class Lab2ApplicationTests {
 
     @Test
